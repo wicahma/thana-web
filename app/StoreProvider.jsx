b295ff8b2a65da8dc1666ba88d0d2b70fe690b84
wicahma <teguh.dc.kusuma@gmail.com>
2024-08-25 11:00:53 +0700

    update: managing store folder & assign Provider to layout

--- app/StoreProvider.jsx
+++ app/StoreProvider.jsx
@@ -1,7 +1,8 @@
 "use client";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Provider } from "react-redux";
-import { makeStore } from "../lib/store";
+import { makeStore } from "../lib/store/store";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export default function StoreProvider({ children }) {
   const storeRef = useRef();
@@ -9,6 +10,15 @@ export default function StoreProvider({ children }) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
   }
+  
+  useEffect(() => {
+    if (storeRef.current != null) {
+      // configure listeners using the provided defaults
+      // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
+      const unsubscribe = setupListeners(storeRef.current.dispatch);
+      return unsubscribe;
+    }
+  }, []);
 
   return <Provider store={storeRef.current}>{children}</Provider>;
 }
--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,33 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Map from "../components/map";
 import "./globals.css";
+import SideCardDetail from "../components/SideCardDetail";
+import StoreProvider from "./StoreProvider";
 
-export default function RootLayout({ children, dashboard, data, login, user }) {
+export default function RootLayout({ children, params }) {
   const [pos, setPos] = useState([-7.8381006, 110.3879432]);
+
+  useEffect(() => {
+    console.log("Ini paramnya", params);
+  }, [params]);
+
   return (
-    <html lang="id">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Phantox Negara</title>
-      </head>
-      <body>
-        <div className="h-full w-full">
-          {children}
-          {/* <div>{dashboard}</div> */}
-          <div>{data}</div>
-          {/* <div>{login}</div> */}
-          {/* <div>{user}</div>{" "} */}
-        </div>
-        <Map />
-      </body>
-    </html>
+    <StoreProvider>
+      <html lang="id">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Phantox Negara</title>
+        </head>
+        <body>
+          <div className="relative">
+            <SideCardDetail />
+          </div>
+          <div className="h-full w-full">{children}</div>
+          <Map />
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
