088bd267965041a07fa683b90d787216ceef8d56
wicahma <teguh.dc.kusuma@gmail.com>
2024-08-25 07:35:40 +0700

    update: creating transition effect for dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out": {
+          "0%": { opacity: "1", transform: "translateY(0)" },
+          "100%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.3s ease-out forwards",
+        "fade-out": "fade-out 0.3s ease-out forwards",
+      },
     },
   },
   plugins: [],
