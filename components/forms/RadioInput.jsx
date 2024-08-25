e7386b11b5cb9c6ad5f4918bac20418c71d66e05
wicahma <teguh.dc.kusuma@gmail.com>
2024-08-25 07:36:04 +0700

    feat: adding more component form for native form handling

--- components/forms/RadioInput.jsx
+++ components/forms/RadioInput.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 
-const RadioInput = ({
-  placeholder = "Masukkan teks",
-  name,
-  className,
-  value,
-  setValue,
-  type = "text",
-}) => {
+const RadioInput = ({ name, className, value, setValue, initValues }) => {
   return (
-    <div className={`flex gap-2 items-center ${className}`}>
+    <div className={`space-y-2 ${className}`}>
       <p className="font-light text-sm">{name}</p>
-      <input
-        value={value}
-        placeholder={placeholder}
-        onChange={(e) => setValue(e.target.value)}
-        className="border grow border-gray-500 rounded-md px-2 py-1"
-        type={type}
-      />
+      <fieldset className="break-keep text-nowrap" onChange={(e) => setValue(e.target.value)} id={name}>
+        {initValues.constructor === Object &&
+          Object.entries(initValues).map(([key, val], i) => (
+            <div key={`${i}-${key}`} className="flex gap-3 w-fit">
+              <input
+                value={val}
+                name={name}
+                className="border grow border-gray-500 rounded-md px-2 py-1"
+                type="radio"
+              />
+              <label htmlFor={val}>{val}</label>
+            </div>
+          ))}
+        {initValues.constructor === Array &&
+          initValues.map((val, i) => (
+            <div key={`${i}-${val}`}>
+              <input
+                value={val}
+                name={name}
+                className="border grow border-gray-500 rounded-md px-2 py-1"
+                type="radio"
+              />
+              <label htmlFor={val}>{val}</label>
+            </div>
+          ))}
+      </fieldset>
     </div>
   );
 };
--- components/forms/index.js
+++ components/forms/index.js
@@ -1,2 +1,4 @@
 export { default as MainInput } from "./MainInput";
 export { default as DropdownInput } from "./DropdownInput";
+export { default as RadioInput } from "./RadioInput";
+export { default as TextAreaInput } from "./TextAreaInput";
