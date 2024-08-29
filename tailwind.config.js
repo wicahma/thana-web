cb389eac620488aa7e9fe26d275c24f8016f3421
wicahma <teguh.dc.kusuma@gmail.com>
2024-08-29 09:42:21 +0700

    feat: disabling strictmode & some rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./providers/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       keyframes: {
-        "fade-in": {
+        "slide-in": {
           "0%": {
             opacity: "0",
             transform: "translateY(-10px)",
@@ -18,17 +19,27 @@ module.exports = {
             transform: "translateY(0)",
           },
         },
-        "fade-out": {
+        "slide-out": {
           "0%": { opacity: "1", transform: "translateY(0)" },
           "100%": {
             opacity: "0",
             transform: "translateY(-10px)",
           },
         },
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        "fade-out": {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
       },
       animation: {
         "fade-in": "fade-in 0.3s ease-out forwards",
         "fade-out": "fade-out 0.3s ease-out forwards",
+        "slide-in": "slide-in 0.3s ease-out forwards",
+        "slide-out": "slide-out 0.3s ease-out forwards",
       },
     },
   },
